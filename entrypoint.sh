affec6df452719b105378d784c4cd0efdebd227a
Tomer Admon <tomer.a@qualisystems.com>
2023-12-03 20:55:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ ENV_FILE="$2"
 
 echo "Running torque plan environment command"
 
-command="cat ${ENV_FILE} | /Quali.Torque.Cli/torque eac plan ${ENV_ID} --output=json -d"
+command="cat ${ENV_FILE} | /Quali.Torque.Cli/torque eac plan ${ENV_ID}"
 echo "The following command will be executed: ${command}"
 
 $command
