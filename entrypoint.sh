aadca9405ee95b4a75f07452ff43a3e3d7c0aa48
Tomer Admon <tomer.a@qualisystems.com>
2023-12-03 19:58:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,12 @@
 #!/bin/sh -l
 
-BP_NAME="$1"
-REPO_NAME="$2"
-ENV_NAME="$3"
-BRANCH="$4"
-DURATION="$5"
-TIMEOUT="$6"
-INPUTS="$7"
+ENV_ID="$3"
 
-ENV_NAME="${ENV_NAME:-$BP_NAME-build-$GITHUB_RUN_NUMBER}"
+echo "Running torque plan environment command"
 
-echo "Running torque start environment command"
-params="\"${BP_NAME}\" --repo \"${REPO_NAME}\" -n \"${ENV_NAME}\" -d \"${DURATION}\""
-
-if [ "$TIMEOUT" -gt 0 ]; then
-    params="$params -w -t ${TIMEOUT}"
-fi
-if [ ! -z "${INPUTS}" ]; then
-    params="$params -i \"${INPUTS}\""
-fi
-if [ ! -z "${BRANCH}" ]; then
-    params="$params -b \"${BRANCH}\""
-fi
-
-command="torque --disable-version-check env start ${params} --output=json"
+command="torque eac plan ${ENV_ID} --output=json -d"
 echo "The following command will be executed: ${command}"
 
 echo "Starting the environment..."
 response=$(eval $command) || exit 1
-# response=$(torque --disable-version-check env start ${params} --output=json) || exit 1
-environment_id=$(echo "$response" | tr -d '"')
-echo "Started environment with id '${environment_id}'"
 
-response=$(torque --disable-version-check env get ${environment_id} --output=json --detail) || exit 1
-environment_details=$(echo "$response" | tr -d "\n")
-
-echo "Writing data to outputs"
-echo "environment_id=${environment_id}" >> $GITHUB_OUTPUT
-echo "environment_details=${environment_details}" >> $GITHUB_OUTPUT
