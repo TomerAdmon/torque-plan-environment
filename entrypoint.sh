32ae8ee33ab2f68812ca00a2f09caf9dcbc49d91
Tomer Admon <tomer.a@qualisystems.com>
2023-12-03 20:26:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,4 @@ echo "Running torque plan environment command"
 command="torque eac plan ${ENV_ID} --output=json -d"
 echo "The following command will be executed: ${command}"
 
-echo "look for Torque?"
-ls -l
-
-echo "Starting the environment..."
-response=$(eval $command) || exit 1
-
+$command
